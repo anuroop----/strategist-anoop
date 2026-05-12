3bad624ce2b1521d52b2264c8f04c6ce832c37c8
anuroop---- <avapanoop2000@gmail.com>
2026-05-12 19:37:38 +0530

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
+  title: "Anoop Varghese — Mobile & Web Developer",
   author: "Anoop Varghese",
   description:
     "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
